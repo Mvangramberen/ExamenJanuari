7943eb027ea7bda0cc0c1a16be7844d57c051dbf
Mickaël Vangramberen <m.vangramberen@hotmail.com>
2017-01-25 16:08:01 +0100

    Main methode
    main methode definereren zodat er 2 patienten geregistreerd worden.

--- ProjectExamenVraag1/src/patientRest/PatientClient.java
+++ ProjectExamenVraag1/src/patientRest/PatientClient.java
@@ -6,36 +6,47 @@ import org.restlet.resource.ClientResource;
 
 public class PatientClient {
 
-    // http://hc.apache.org/
-     public static void main(String[] args) {
-           
-             try {
-            	// Post a new record
-            	JSONObject json = new JSONObject();
-        		json.put("lastname", "Doe");
-        		json.put("firstname", "John");
-        		json.put("uur interventie", 20);
-        		json.put("geboorteDatum", "01/01/1990");
-        		json.put("naamVerpleegkundige", "Dctr. Vanpuynbroek");
-        		json.put("diagnose", "Schedelbreuk");
-        		PatientResource.post(json.toString());
-        		
 
-            	
-            	// Write the response entity on the console
-            	PatientResource.get().write(System.out);
-            	System.out.println();
-            	// Process the resource
-                JSONObject obj1 = new JSONObject(PatientResource.get(String.class));
-                JSONArray array1 = obj1.getJSONArray("result");
-               
-                for(int i = 0; i < array1.length(); i++) {
-                    JSONObject obj2 = array1.getJSONObject(i);
-                    System.out.println(obj2.toString());
-                 }
-            }
-            catch (Exception e) {
-                System.out.println("In main : " + e.getMessage());
-            }
+     
+     
+     // Methode aanmaken voor het registreren van patienten
+     public void patientRegistreren(String voornaam,String achternaam, int UI,String geboorteDatum, String verpleegkundige,String diagnose){
+    	
+    	 try {
+         	// Post a new record
+         	JSONObject json = new JSONObject();
+     		json.put("lastname", achternaam);
+     		json.put("firstname", voornaam);
+     		json.put("uur interventie", UI);
+     		json.put("geboorteDatum", geboorteDatum);
+     		json.put("naamVerpleegkundige", verpleegkundige);
+     		json.put("diagnose", diagnose);
+     		PatientResource.post(json.toString());
+     		
+
+         	
+         	// Write the response entity on the console
+         	PatientResource.get().write(System.out);
+         	System.out.println();
+         	// Process the resource
+             JSONObject obj1 = new JSONObject(PatientResource.get(String.class));
+             JSONArray array1 = obj1.getJSONArray("result");
+            
+             for(int i = 0; i < array1.length(); i++) {
+                 JSONObject obj2 = array1.getJSONObject(i);
+                 System.out.println(obj2.toString());
+              }
+         }
+         catch (Exception e) {
+             System.out.println("In main : " + e.getMessage() +"\nEr is een fout opgetreden bij het registreren van een patient!");
+         }
+    	 
+     }
+
+     public static void main(String[] args) {
+    	 patientRegistreren("John","Doe", 20,"01/01/1990", "Docter Vanpuynbroek","Schedelfractuur");
+    	 patientRegistreren("John","Doe", 20,"01/01/1990", "Docter Vanpuynbroek","Schedelfractuur");
+
         }
-    }
+}
+
